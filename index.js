927af1b67a4d7eafe7e89b5da4040740c94b3a8d
asimsedhain <asimsedhain33@gmail.com>
2020-08-28 03:39:35 -0500

    debug: output prefix

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ let PomodoroCount = {};
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Start with ${PREFIX}`)
 	//let job = new CronJob(
 		//"0 0 0 * * *",
 		//dailyWinner,
